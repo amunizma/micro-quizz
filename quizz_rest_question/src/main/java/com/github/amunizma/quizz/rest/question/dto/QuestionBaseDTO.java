199ddee590a7329ad1ee61bdd62181cd57a2636c
Ana Muñiz <89533690+amunizma@users.noreply.github.com>
2024-09-28 23:36:23 +0200

    Pruebas JUnit Controlador

--- quizz_rest_question/src/main/java/com/github/amunizma/quizz/rest/question/dto/QuestionBaseDTO.java
+++ quizz_rest_question/src/main/java/com/github/amunizma/quizz/rest/question/dto/QuestionBaseDTO.java
@@ -20,13 +20,18 @@ import lombok.experimental.SuperBuilder;
 @SuperBuilder
 public class QuestionBaseDTO {
 	
+	/**
+	 * @NotBlank: se usa en cadenas; y comprueba que no sean nulas, ni vacía, ni contenga solo espacios en blancos
+	 */
 	@NotBlank(message = Constant.LEVEL_BAD_REQUEST) //Cadena no nula ni vacia
-	@NotNull(message = Constant.LEVEL_BAD_REQUEST)
 	private String level;
 	
 	@NotBlank(message = Constant.DIFFICULTY_BAD_REQUEST)
 	private String difficulty;
 	
+	/**
+	 * @NotBlank: se usa en cadenas, colecciones, arrays y mapas; y comprueba que no sea nulo, ni vacío
+	 */
 	@NotEmpty(message = Constant.QUESTION_BAD_REQUEST) //Lista no nula ni vacia
 	private List<String> questions;
 	
--- quizz_rest_question/src/test/java/com/github/amunizma/quizz/rest/question/controller/QuestionControllerTest.java
+++ quizz_rest_question/src/test/java/com/github/amunizma/quizz/rest/question/controller/QuestionControllerTest.java
@@ -1,18 +1,21 @@
 package com.github.amunizma.quizz.rest.question.controller;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -22,9 +25,11 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.github.amunizma.quizz.rest.question.controller.QuestionController;
 import com.github.amunizma.quizz.rest.question.dto.QuestionBaseDTO;
 import com.github.amunizma.quizz.rest.question.dto.QuestionDTO;
+import com.github.amunizma.quizz.rest.question.exception.BadRequestException;
+import com.github.amunizma.quizz.rest.question.exception.ConflictException;
+import com.github.amunizma.quizz.rest.question.exception.InternalServerErrorException;
 import com.github.amunizma.quizz.rest.question.service.QuestionService;
 
 @WebMvcTest(QuestionController.class)
@@ -32,7 +37,7 @@ public class QuestionControllerTest {
 	@Autowired
     private MockMvc mockMvc;
 
-    @Mock
+    @MockBean
     private QuestionService questionService;
 
     @InjectMocks
@@ -41,23 +46,25 @@ public class QuestionControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
     
+    
+    private QuestionBaseDTO questionBaseDTO;
+    
+    private QuestionDTO questionDTO ;
+    
     @BeforeEach
     public void setup() {
         MockitoAnnotations.openMocks(this);
         this.mockMvc = MockMvcBuilders.standaloneSetup(questionController).build();
-    }
-    
-    @Test
-    public void completQuestionBaseDTO_createQuestion_Created() throws Exception {
-    	QuestionBaseDTO questionBaseDTO = new QuestionBaseDTO().builder()
+        
+		questionBaseDTO = QuestionBaseDTO.builder()
     			.level("A")
     			.difficulty("1")
     			.questions(Arrays.asList("el mejor amigo del hombre"))
     			.correctAnswers(Arrays.asList("El perro"))
     			.wrongAnswers(Arrays.asList("El gato","el koala","el caballo"))
     			.build();
-    	
-    	QuestionDTO questionDTO = new QuestionDTO().builder()
+        
+        questionDTO = QuestionDTO.builder()
     			.level("A")
     			.difficulty("1")
     			.questions(Arrays.asList("el mejor amigo del hombre"))
@@ -66,7 +73,11 @@ public class QuestionControllerTest {
     			.questionId("5846002f-b929-4714-aa64-27f0db3508bd")
     			.used(null)
     			.build();
-        
+    }
+    
+    @Test
+    public void completQuestionBaseDTO_createQuestion_Created() throws Exception {
+
         when(questionService.createQuestion(any(QuestionBaseDTO.class))).thenReturn(questionDTO);
 
         mockMvc.perform(post("/question")
@@ -75,5 +86,140 @@ public class QuestionControllerTest {
                 .andExpect(status().isCreated());
     }
     
+    @Test
+    public void completQuestionBaseDTO_createQuestion_conflictException() throws Exception {
+    	
+    	doThrow(new ConflictException("Conflict Exception"))
+        	.when(questionService)
+        	.createQuestion(any(QuestionBaseDTO.class)); 
+
+        mockMvc.perform(post("/question")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(questionBaseDTO)))
+                .andExpect(status().isConflict());
+    }
+    
+    @Test
+    public void completQuestionBaseDTO_createQuestion_badRequestException() throws Exception {
+
+    	doThrow(new BadRequestException("Bad Request Exception"))
+        	.when(questionService)
+        	.createQuestion(any(QuestionBaseDTO.class)); 
+
+        mockMvc.perform(post("/question")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(questionBaseDTO)))
+                .andExpect(status().isBadRequest());
+    }
+    
+    @Test
+    public void completQuestionBaseDTO_createQuestion_internalServerErrorException() throws Exception {
+
+    	doThrow(new InternalServerErrorException("Internal Server Error Exception"))
+        	.when(questionService)
+        	.createQuestion(any(QuestionBaseDTO.class)); 
+
+        mockMvc.perform(post("/question")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(questionBaseDTO)))
+                .andExpect(status().isInternalServerError());
+    }
+    
+    
+    @ParameterizedTest
+    @CsvSource({
+        "null", 
+        "''", //Representa una cadena vacía
+        "'    '"
+    })
+    public void incompletQuestionBaseDTOLevel_createQuestion_badRequestException(String level) throws Exception {
+    	if ("null".equals(level)) {
+    		level = null;
+        }
+    	questionBaseDTO.setLevel(level);
+        
+        when(questionService.createQuestion(any(QuestionBaseDTO.class))).thenReturn(questionDTO);
+ 
+
+        mockMvc.perform(post("/question")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(questionBaseDTO)))
+                .andExpect(status().isBadRequest());
+                //.andExpect(content().string(Constant.LEVEL_BAD_REQUEST));
+    }
+    
+    @ParameterizedTest
+    @CsvSource({
+        "null", 
+        "''", //Representa una cadena vacía
+        "'    '"
+    })
+    public void incompletQuestionBaseDTODifficulty_createQuestion_badRequestException(String difficulty) throws Exception {
+    	if ("null".equals(difficulty)) {
+    		difficulty = null;
+        }
+    	questionBaseDTO.setDifficulty(difficulty);
+        
+        when(questionService.createQuestion(any(QuestionBaseDTO.class))).thenReturn(questionDTO);
+ 
+
+        mockMvc.perform(post("/question")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(questionBaseDTO)))
+                .andExpect(status().isBadRequest());
+                //.andExpect(content().string(Constant.DIFFICULTY_BAD_REQUEST));
+    }
+    
+    @ParameterizedTest
+    @MethodSource("listParameterized")
+    public void incompletQuestionBaseDTOQuestions_createQuestion_badRequestException(List<String> questions) throws Exception {
+    	questionBaseDTO.setQuestions(questions);
+        
+        when(questionService.createQuestion(any(QuestionBaseDTO.class))).thenReturn(questionDTO);
+ 
+
+        mockMvc.perform(post("/question")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(questionBaseDTO)))
+                .andExpect(status().isBadRequest());
+                //.andExpect(content().string(Constant.QUESTION_BAD_REQUEST));
+    }
+    
+    @ParameterizedTest
+    @MethodSource("listParameterized")
+    public void incompletQuestionBaseDTOCorrectAnswers_createQuestion_badRequestException(List<String> correctAnswers) throws Exception {
+    	questionBaseDTO.setCorrectAnswers(correctAnswers);
+        
+        when(questionService.createQuestion(any(QuestionBaseDTO.class))).thenReturn(questionDTO);
+ 
+
+        mockMvc.perform(post("/question")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(questionBaseDTO)))
+                .andExpect(status().isBadRequest());
+                //.andExpect(content().string(Constant.CORRECT_ANSWER_BAD_REQUEST));
+    }
+    
+    @ParameterizedTest
+    @MethodSource("listParameterized")
+    public void incompletQuestionBaseDTOWrongAnswers_createQuestion_badRequestException(List<String> wrongAnswers) throws Exception {
+    	questionBaseDTO.setCorrectAnswers(wrongAnswers);
+        
+        when(questionService.createQuestion(any(QuestionBaseDTO.class))).thenReturn(questionDTO);
+ 
+
+        mockMvc.perform(post("/question")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(questionBaseDTO)))
+                .andExpect(status().isBadRequest());
+                //.andExpect(content().string(Constant.WRONG_ANSWER_BAD_REQUEST));
+    }
+    
+    static Collection<List<String>> listParameterized() {
+        return Arrays.asList(
+            Arrays.asList(),
+            null
+        );
+    }
     
 }
