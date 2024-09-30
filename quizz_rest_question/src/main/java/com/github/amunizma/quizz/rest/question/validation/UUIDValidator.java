ef3d60fcbcb483b5932446ed793acfa1fd2653a2
Ana Muñiz <89533690+amunizma@users.noreply.github.com>
2024-09-30 18:56:13 +0200

    implementación getQuestion

---
+++ quizz_rest_question/src/main/java/com/github/amunizma/quizz/rest/question/validation/UUIDValidator.java
@@ -0,0 +1,22 @@
+package com.github.amunizma.quizz.rest.question.validation;
+
+import java.util.UUID;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class UUIDValidator implements ConstraintValidator<ValidUUID, String> {
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+    	boolean valid = false;
+        try {
+        	if (value != null && !"".equals(value.trim())) {
+	            UUID.fromString(value);
+	            valid = true;
+        	}
+        } catch (IllegalArgumentException e) {
+        }
+        return valid;
+    }
+}
