4fc4ecf5a1bb915e4d1c0d7e7626e54e11387bb1
Ana Muñiz <89533690+amunizma@users.noreply.github.com>
2024-09-30 18:56:27 +0200

    JUnit

--- quizz_rest_question/src/test/java/com/github/amunizma/quizz/rest/question/service/QuestionServiceTest.java
+++ quizz_rest_question/src/test/java/com/github/amunizma/quizz/rest/question/service/QuestionServiceTest.java
@@ -1,6 +1,8 @@
 package com.github.amunizma.quizz.rest.question.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doThrow;
@@ -9,6 +11,8 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -16,6 +20,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.dao.DataAccessException;
 import org.springframework.dao.DataIntegrityViolationException;
 
 import com.github.amunizma.quizz.rest.question.dto.QuestionBaseDTO;
@@ -23,6 +28,8 @@ import com.github.amunizma.quizz.rest.question.dto.QuestionDTO;
 import com.github.amunizma.quizz.rest.question.entity.QuestionEntity;
 import com.github.amunizma.quizz.rest.question.exception.BadRequestException;
 import com.github.amunizma.quizz.rest.question.exception.InternalServerErrorException;
+import com.github.amunizma.quizz.rest.question.exception.NotFoundException;
+import com.github.amunizma.quizz.rest.question.exception.ServiceUnavailableException;
 import com.github.amunizma.quizz.rest.question.mapper.QuestionMapper;
 import com.github.amunizma.quizz.rest.question.respository.QuestionRepository;
 import com.mongodb.MongoException;
@@ -45,39 +52,48 @@ class QuestionServiceTest {
     private QuestionBaseDTO questionBaseDTO;
     private QuestionDTO questionDTO;
     private QuestionEntity questionEntity;
+    private String idQuestion;
     
     @BeforeEach
     void setup() {
+       idQuestion = "5846002f-b929-4714-aa64-27f0db3508bd";
+       String level = "A";
+       String difficulty = "1";
+       List<String> questions = Arrays.asList("el mejor amigo del hombre");
+       List<String> correctAnswers = Arrays.asList("El perro");
+       List<String> wrongAnswers = Arrays.asList("El gato","el koala","el caballo");
        
 		questionBaseDTO = QuestionBaseDTO.builder()
-    			.level("A")
-    			.difficulty("1")
-    			.questions(Arrays.asList("el mejor amigo del hombre"))
-    			.correctAnswers(Arrays.asList("El perro"))
-    			.wrongAnswers(Arrays.asList("El gato","el koala","el caballo"))
+    			.level(level)
+    			.difficulty(difficulty)
+    			.questions(questions)
+    			.correctAnswers(correctAnswers)
+    			.wrongAnswers(wrongAnswers)
     			.build();
         
         questionDTO = QuestionDTO.builder()
-    			.level("A")
-    			.difficulty("1")
-    			.questions(Arrays.asList("el mejor amigo del hombre"))
-    			.correctAnswers(Arrays.asList("El perro"))
-    			.wrongAnswers(Arrays.asList("El gato","el koala","el caballo"))
-    			.questionId("5846002f-b929-4714-aa64-27f0db3508bd")
+        		.level(level)
+    			.difficulty(difficulty)
+    			.questions(questions)
+    			.correctAnswers(correctAnswers)
+    			.wrongAnswers(wrongAnswers)
+    			.questionId(idQuestion)
     			.used(null)
     			.build();
         
         questionEntity = QuestionEntity.builder()
-    			.level("A")
-    			.difficulty("1")
-    			.questions(Arrays.asList("el mejor amigo del hombre"))
-    			.correctAnswers(Arrays.asList("El perro"))
-    			.wrongAnswers(Arrays.asList("El gato","el koala","el caballo"))
-    			.questionId("5846002f-b929-4714-aa64-27f0db3508bd")
+        		.level(level)
+    			.difficulty(difficulty)
+    			.questions(questions)
+    			.correctAnswers(correctAnswers)
+    			.wrongAnswers(wrongAnswers)
+    			.questionId(idQuestion)
     			.used(null)
     			.build();
+        
     }
     
+    /** createQuestion() */
     @Test
     void completQuestionBaseDTO_createQuestion_success() throws Exception {
 
@@ -117,5 +133,72 @@ class QuestionServiceTest {
     	
         assertThrows(InternalServerErrorException.class, () -> questionService.createQuestion(questionBaseDTO));
     }
+    
+    /** getQuestion(String id) */
+    
+    @Test
+    void completId_getQuestion_success() {
+        when(questionRepository.findById(idQuestion)).thenReturn(Optional.of(questionEntity));
+        when(questionMapper.toQuestionDTO(questionEntity)).thenReturn(questionDTO);
+
+        QuestionDTO result = questionService.getQuestion(idQuestion);
+
+        assertNotNull(result);
+        assertEquals(questionDTO, result);
+
+        // Verifica que los métodos del repositorio y el mapper fueron llamados
+        verify(questionRepository, times(1)).findById(idQuestion);
+        verify(questionMapper, times(1)).toQuestionDTO(questionEntity);
+    }
+    
+    @Test
+    void completId_getQuestion_notFound() {
+        when(questionRepository.findById(idQuestion)).thenReturn(Optional.empty());
+
+        // Verifica que se lance la excepción NotFoundException
+        NotFoundException exception = assertThrows(NotFoundException.class, () -> {
+        	questionService.getQuestion(idQuestion);
+        });
+
+        assertEquals("question with questionId not found", exception.getMessage());
+
+        // Verifica que el método findById fue llamado pero no el mapper
+        verify(questionRepository, times(1)).findById(idQuestion);
+        verify(questionMapper, times(0)).toQuestionDTO(any(QuestionEntity.class));
+    }
    
+    @Test
+    void completId_getQuestion_serviceUnavailableException() {
+        when(questionRepository.findById(idQuestion)).thenThrow(new DataAccessException("DB access error") {});
+
+        // Verifica que se lance la excepción NotFoundException
+        ServiceUnavailableException exception = assertThrows(ServiceUnavailableException.class, () -> {
+        	questionService.getQuestion(idQuestion);
+        });
+
+        //assertEquals("question with questionId not found", exception.getMessage());
+        assertNull(exception.getMessage());
+
+        // Verifica que el método findById fue llamado pero no el mapper
+        verify(questionRepository, times(1)).findById(idQuestion);
+        verify(questionMapper, times(0)).toQuestionDTO(any(QuestionEntity.class));
+    }
+    
+    @Test
+    void completId_getQuestion_internalServerErrorException() {
+        when(questionRepository.findById(idQuestion)).thenThrow(new MongoException("Mongo Exception error") {});
+
+        // Verifica que se lance la excepción NotFoundException
+        InternalServerErrorException exception = assertThrows(InternalServerErrorException.class, () -> {
+        	questionService.getQuestion(idQuestion);
+        });
+
+        //assertEquals("question with questionId not found", exception.getMessage());
+        assertNull(exception.getMessage());
+
+        // Verifica que el método findById fue llamado pero no el mapper
+        verify(questionRepository, times(1)).findById(idQuestion);
+        verify(questionMapper, times(0)).toQuestionDTO(any(QuestionEntity.class));
+    }
+ 
 }
